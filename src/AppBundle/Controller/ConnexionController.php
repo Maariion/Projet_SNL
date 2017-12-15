622f8e7b9e3b2be5ba6355ebcb7c325a697a335b
pidjeygphy <pjfonchais@gmail.com>
2017-12-15 11:23:28 +0100

    merge pj

---
+++ src/AppBundle/Controller/ConnexionController.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Pierre-Joseph
+ * Date: 15/12/2017
+ * Time: 11:17
+ */
+
+namespace AppBundle\Controller;
+
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+
+class ConnexionController extends Controller
+{
+    /**
+     * @Route("/", name="index")
+     */
+    public function indexAction(Request $request)
+    {
+        // replace this example code with whatever you need
+        return $this->render('default/index.html.twig', [
+            'base_dir' => realpath($this->getParameter('kernel.project_dir')).DIRECTORY_SEPARATOR,
+        ]);
+    }
+}
