8e93672fd2c63cd587b12823bc9cf7341314919e
Mihawk24 <moriss.clement@gmail.com>
2018-01-22 15:03:21 +0100

    Correction du controller client

--- src/AppBundle/Controller/ClientController.php
+++ src/AppBundle/Controller/ClientController.php
@@ -20,17 +20,7 @@ class ClientController extends Controller
      */
     public function indexAction(Request $request)
     {
-        $clients = $this->getDoctrine()
-            ->getRepository('AppBundle:Utilisateur')
-            ->findAll();
-
-        if(!$clients){
-            throw $this->createNotFoundException(
-                "Aucun client n'a été trouvé"
-            );
-        }
-
         // replace this example code with whatever you need
-        return $this->render('default/client.html.twig', array('clients'=>$clients));
+        return $this->render('default/client.html.twig');
     }
 }
--- src/AppBundle/Controller/SoftnlabsClientPartController.php
+++ src/AppBundle/Controller/SoftnlabsClientPartController.php
@@ -19,9 +19,18 @@ class SoftnlabsClientPartController extends Controller
      */
     public function indexAction(Request $request)
     {
+        $clients = $this->getDoctrine()
+            ->getRepository('AppBundle:Utilisateur')
+            ->findAll();
+
+        if(!$clients){
+            throw $this->createNotFoundException(
+                "Aucun client n'a été trouvé"
+            );
+        }
+
         // replace this example code with whatever you need
-        return $this->render('default/softnlabs_client_part.html.twig', [
-            'base_dir' => realpath($this->getParameter('kernel.project_dir')).DIRECTORY_SEPARATOR,
-        ]);
+        return $this->render('default/softnlabs_client_part.html.twig', array('clients'=>$clients));
+
     }
 }
