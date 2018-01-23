572f185bf9b3ea4e34a5227fcf8e65f3c619d949
Mihawk24 <moriss.clement@gmail.com>
2018-01-23 09:09:32 +0100

    Modification des rendus visuels de la partie SNL + tentative de tableau se triant pour les clients

---
+++ src/AppBundle/Form/TicketAddType.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace AppBundle\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class TicketAddType extends AbstractType
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('titre')
+            ->add('description')
+            ->add('tpsprisecompte')
+            ->add('tpsresolution')
+            ->add('idtyp')
+            ->add('idsta')
+            ->add('idcrit')
+            ->add('idutilClient')
+            ->add('idutilConsultant')
+            ->add('idsys');
+    }
+    
+    /**
+     * {@inheritdoc}
+     */
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults(array(
+            'data_class' => 'AppBundle\Entity\Ticket'
+        ));
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getBlockPrefix()
+    {
+        return 'appbundle_ticket';
+    }
+
+
+}
