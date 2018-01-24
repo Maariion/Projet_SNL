160fbe887d591db3e994653d34dd50ec36abe8e9
Marion <marion.serazin@hotmail.com>
2018-01-24 10:15:50 +0100

    test ms

--- src/AppBundle/Controller/SoftnlabsClientPartController.php
+++ src/AppBundle/Controller/SoftnlabsClientPartController.php
@@ -25,6 +25,15 @@ class SoftnlabsClientPartController extends Controller
 
         // replace this example code with whatever you need
         return $this->render('default/softnlabs_client_part.html.twig', array('clients'=>$clients));
+    }
 
+    public function myFindDQL($nomU, $nomO, $role)
+    {
+        $query =$this->_em->createQuery('select u.nom, o.nom, u.role from organisation o join utilisateur u on o.IDOrg=u.IDOrg');
+        $query->setParameter('o.nom', $nomO);
+        return $query
+            ->getQuery()
+            ->getResult()
+            ;
     }
 }
