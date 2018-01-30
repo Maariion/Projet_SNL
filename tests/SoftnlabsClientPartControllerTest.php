cdd8a2e26df4243c0fcc517d39195eeec906d570
Marion <marion.serazin@hotmail.com>
2018-01-30 16:57:05 +0100

    test ms

---
+++ tests/SoftnlabsClientPartControllerTest.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Marion
+ * Date: 30/01/2018
+ * Time: 11:15
+ */
+
+use AppBundle\Controller\SoftnlabsClientPartController;
+use PHPUnit\Framework\TestCase;
+
+class SoftnlabsClientPartControllerTest extends TestCase
+{
+
+
+
+}
---
+++ tests/SoftnlabsTicketPartControllerTest.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Marion
+ * Date: 30/01/2018
+ * Time: 11:24
+ */
+
+namespace Tests;
+
+use AppBundle\Controller\SoftnlabsTicketPartController;
+
+class SoftnlabsTicketPartControllerTest extends \PHPUnit_Framework_TestCase
+{
+
+}
