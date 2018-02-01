918e41e3c4827db0b47e0e7559af8be94c20fa57
pidjeygphy <pjfonchais@gmail.com>
2018-02-01 15:18:12 +0100

    Ajout de package lors de la connexion

--- src/AppBundle/Controller/ConnexionController.php
+++ src/AppBundle/Controller/ConnexionController.php
@@ -13,6 +13,8 @@ use AppBundle\Form\UtilisateurType;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Session\Session;
 
 class ConnexionController extends Controller
 {
